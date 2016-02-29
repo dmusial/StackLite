bea8e3d46968fc8c019355cf89c0bd6b559daec8
Grabowski <sylwester.grabowski@capgemini.com>
2016-02-29 10:52:30 +0100

    bower files

--- StackLite.Core/StackLite.Core.UI/gulpfile.js
+++ StackLite.Core/StackLite.Core.UI/gulpfile.js
@@ -1,5 +1,33 @@
 var gulp = require('gulp'),
-    plugins = require("gulp-load-plugins")()
+    plugins = require("gulp-load-plugins")(),
+    mainBowerFiles = require("main-bower-files")
+//mainBowerFiles
+
+
+gulp.task('libjs', function() {
+    return gulp.src(mainBowerFiles())
+        .pipe(plugins.filter(["*.js"]))
+        .pipe(plugins.concat('lib.js'))
+        .pipe(gulp.dest("wwwroot"))
+        .pipe(plugins.rename({
+            suffix: ".min"
+        }))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest("wwwroot"));
+});
+
+gulp.task('libcss', function() {
+    return gulp.src(mainBowerFiles())
+        .pipe(plugins.filter(["*.css"]))
+        .pipe(plugins.concat('lib.css'))
+        .pipe(gulp.dest("wwwroot"))
+        .pipe(plugins.rename({
+            suffix: ".min"
+        }))
+        .pipe(plugins.cssnano())
+        .pipe(gulp.dest("wwwroot"));
+});
+
 
 gulp.task('buildjs', function() {
     return gulp.src(["app/src/**/site.js", "app/src/**/*.js"])
@@ -37,6 +65,6 @@ gulp.task('watch',['default'], function() {
   gulp.watch("app/src/**/*.html", ['copyhtml']);
 });
 
-gulp.task('default', ['buildjs', 'copyhtml','buildcss'], function() {
+gulp.task('default', ['buildjs', 'copyhtml','buildcss','libcss','libjs'], function() {
 
 });
