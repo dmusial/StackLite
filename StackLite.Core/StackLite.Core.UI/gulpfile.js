57be42963d26ba7401b71fb9515fccde38948577
Grabowski <sylwester.grabowski@capgemini.com>
2016-02-29 10:10:36 +0100

    added gulp concat

--- StackLite.Core/StackLite.Core.UI/gulpfile.js
+++ StackLite.Core/StackLite.Core.UI/gulpfile.js
@@ -1,8 +1,9 @@
-var gulp = require('gulp');
-
+var gulp = require('gulp'),
+    concat = require('gulp-concat')
 
 gulp.task('buildjs',function(){
   return gulp.src(["app/src/**/*.js"])
+             .pipe(concat('app.js'))
              .pipe(gulp.dest("wwwroot"));
 });
 
